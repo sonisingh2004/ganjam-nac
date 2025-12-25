ee83bb7efe817ab4dd3d01af3e9c5db933419873
BibhuPrasadBaliarsingh <bibhuprasadbaliarsingh@gmail.com>
2025-12-25 19:04:21 +0530

    updatesomething

--- clients/src/routes/AppRoutes.jsx
+++ clients/src/routes/AppRoutes.jsx
@@ -1,9 +1,10 @@
 // src/routes/AppRoutes.jsx
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import React from "react";
-import MainLayout from "../layout/MainLayout";
-import Dashboard from "../pages/Dashboard";
+
+import AdminLayout from "../layout/AdminLayout";
 import Login from "../pages/Login";
+import Complaint from "../pages/admin/Complaint";
+import AdminDashboard from "../pages/admin/Dashboard";
 
 
 export default function AppRoutes() {
@@ -11,11 +12,24 @@ export default function AppRoutes() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route element={<MainLayout />}>
-          
-          
+        
+        {/* Admin Routes */}
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<AdminDashboard />} />
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="complaints" element={<Complaint />} />
+          {/* Add more admin routes here */}
         </Route>
 
+        {/* Future: Citizen Routes */}
+        {/* <Route path="/citizen" element={<CitizenLayout />}>
+          <Route path="dashboard" element={<CitizenDashboard />} />
+        </Route> */}
+
+        {/* Future: Supervisor Routes */}
+        {/* <Route path="/supervisor" element={<SupervisorLayout />}>
+          <Route path="dashboard" element={<SupervisorDashboard />} />
+        </Route> */}
         
       </Routes>
     </BrowserRouter>
